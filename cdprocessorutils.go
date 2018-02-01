3210d51c5807b40e7077ce36746e1bdbe762fb4c
Simon Tucker <brotherlogic@gmail.com>
2018-01-31 20:13:43 -0800

    More logging

--- cdprocessorutils.go
+++ cdprocessorutils.go
@@ -4,17 +4,19 @@ import (
 	"fmt"
 
 	"golang.org/x/net/context"
-	
+
 	pbcdp "github.com/brotherlogic/cdprocessor/proto"
 )
 
 func (s *Server) logMissing() {
+	s.Log(fmt.Sprintf("RUNNING MISSING"))
 	m, err := s.GetMissing(context.Background(), &pbcdp.GetMissingRequest{})
 	if err != nil {
 		s.Log(fmt.Sprintf("ERROR getting missing: %v", err))
 		return
 	}
 
+	s.Log(fmt.Sprintf("FOUND %v", len(m.Missing)))
 	if len(m.Missing) > 0 {
 		err := s.gh.recordMissing(m.Missing[0])
 		if err != nil {
