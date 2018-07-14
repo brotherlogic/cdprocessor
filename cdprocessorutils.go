146e6a356218f3fbc750e2f3d864f91a01a6ed68
Simon Tucker <brotherlogic@gmail.com>
2018-07-13 17:28:17 -0700

    Cleared logging

--- cdprocessorutils.go
+++ cdprocessorutils.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"golang.org/x/net/context"
 
 	pbcdp "github.com/brotherlogic/cdprocessor/proto"
@@ -27,14 +25,12 @@ func (s *Server) adjustExisting(ctx context.Context) {
 func (s *Server) logMissing(ctx context.Context) {
 	m, err := s.GetMissing(context.Background(), &pbcdp.GetMissingRequest{})
 	if err != nil {
-		s.Log(fmt.Sprintf("ERROR getting missing: %v", err))
 		return
 	}
 
 	if len(m.Missing) > 0 {
 		err := s.gh.recordMissing(m.Missing[0])
 		if err != nil {
-			s.Log(fmt.Sprintf("ERROR recording missing: %v", err))
 			return
 		}
 	}
