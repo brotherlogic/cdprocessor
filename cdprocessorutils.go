a271aecd2f583e3c2322b942160fe2566ab568ea
Simon Tucker <brotherlogic@gmail.com>
2018-09-10 16:24:57 -0700

    Raise an issue on a nil record. This closes #34

--- cdprocessorutils.go
+++ cdprocessorutils.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"golang.org/x/net/context"
 
 	pbcdp "github.com/brotherlogic/cdprocessor/proto"
@@ -15,9 +17,13 @@ func (s *Server) adjustExisting(ctx context.Context) {
 
 	for _, r := range m.Ripped {
 		rec := s.getter.getRecord(ctx, r.Id)
-		if rec.GetMetadata().FilePath != r.Path {
-			rec.GetMetadata().FilePath = r.Path
-			s.getter.updateRecord(ctx, rec)
+		if rec == nil {
+			s.RaiseIssue(ctx, fmt.Sprintf("Nil record?: %v", r.Id))
+		} else {
+			if rec.GetMetadata().FilePath != r.Path {
+				rec.GetMetadata().FilePath = r.Path
+				s.getter.updateRecord(ctx, rec)
+			}
 		}
 	}
 }
