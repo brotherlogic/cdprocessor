f57643fcaca0cb79a2953eb8a47f2637739d3bb7
Simon Tucker <simon@brotherlogic@gmail>
2019-06-04 18:52:03 -0700

    Added some logging on force

--- cdprocessor_cli/cli.go
+++ cdprocessor_cli/cli.go
@@ -18,54 +18,48 @@ import (
 )
 
 func main() {
-	entries, err := utils.ResolveAll("cdprocessor")
+	ip, port, err := utils.Resolve("cdprocessor")
 
 	if err != nil {
 		log.Fatalf("Unable to locate cdprocessor server: %v", err)
 	}
 
-	for _, e := range entries {
-		if e.Master {
-			conn, _ := grpc.Dial(e.Ip+":"+strconv.Itoa(int(e.Port)), grpc.WithInsecure())
-			defer conn.Close()
+	conn, _ := grpc.Dial(ip+":"+strconv.Itoa(int(port)), grpc.WithInsecure())
+	defer conn.Close()
 
-			registry := pbcdp.NewCDProcessorClient(conn)
+	registry := pbcdp.NewCDProcessorClient(conn)
 
-			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-			defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
 
-			switch os.Args[1] {
-			case "force":
-				val, _ := strconv.Atoi(os.Args[2])
-				resp, err := registry.Force(ctx, &pbcdp.ForceRequest{Type: pbcdp.ForceRequest_RECREATE_LINKS, Id: int32(val)})
-				fmt.Printf("%v and %v\n", resp, err)
-			case "got":
-				resp, err := registry.GetRipped(ctx, &pbcdp.GetRippedRequest{})
+	switch os.Args[1] {
+	case "force":
+		val, _ := strconv.Atoi(os.Args[2])
+		resp, err := registry.Force(ctx, &pbcdp.ForceRequest{Type: pbcdp.ForceRequest_RECREATE_LINKS, Id: int32(val)})
+		fmt.Printf("%v and %v\n", resp, err)
+	case "got":
+		resp, err := registry.GetRipped(ctx, &pbcdp.GetRippedRequest{})
 
-				if err == nil {
-					fmt.Printf("%v: Got %v ripped records\n", e.Identifier, len(resp.GetRipped()))
-					i, _ := strconv.Atoi(os.Args[2])
-					for _, missing := range resp.GetRipped() {
-						if missing.Id == int32(i) {
-							fmt.Printf("%v\n", missing)
-						}
-					}
-				} else {
-					fmt.Printf("%v: Error: %v\n", e.Identifier, err)
-				}
-			case "missing":
-				if e.Master {
-					resp, err := registry.GetMissing(ctx, &pbcdp.GetMissingRequest{})
-
-					if err != nil {
-						log.Fatalf("Error in request: %v", err)
-					}
-
-					for i, missing := range resp.Missing {
-						fmt.Printf("%v. [%v] %v\n", i, missing.GetRelease().Id, missing.GetRelease().Title)
-					}
+		if err == nil {
+			fmt.Printf("%v: Got %v ripped records\n", ip, len(resp.GetRipped()))
+			i, _ := strconv.Atoi(os.Args[2])
+			for _, missing := range resp.GetRipped() {
+				if missing.Id == int32(i) {
+					fmt.Printf("%v\n", missing)
 				}
 			}
+		} else {
+			fmt.Printf("%v: Error: %v\n", ip, err)
+		}
+	case "missing":
+		resp, err := registry.GetMissing(ctx, &pbcdp.GetMissingRequest{})
+
+		if err != nil {
+			log.Fatalf("Error in request: %v", err)
+		}
+
+		for i, missing := range resp.Missing {
+			fmt.Printf("%v. [%v] %v\n", i, missing.GetRelease().Id, missing.GetRelease().Title)
 		}
 	}
 }
