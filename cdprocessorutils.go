46ff7d2e2203e970bdf40657f1ad7fcf8f9a29db
Simon Tucker <simon@brotherlogic@gmail>
2019-07-27 13:36:15 -0700

    Adds simple logging about track skipping

--- cdprocessorutils.go
+++ cdprocessorutils.go
@@ -94,6 +94,7 @@ func (s *Server) makeLinks(ctx context.Context, ID int32, force bool) error {
 					return err
 				}
 			}
+			s.Log(fmt.Sprintf("Skipping %v because it's the wrong format", track))
 		}
 
 		record.GetMetadata().CdPath = fmt.Sprintf("%v%v", s.mp3dir, record.GetRelease().Id)
