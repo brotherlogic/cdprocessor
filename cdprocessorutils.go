0bd3861ed2d63e394fc34a07b6592cbf5e353182
Simon Tucker <brotherlogic@gmail.com>
2025-01-24 15:12:21 -0800

    Don't skip digital stuff

--- cdprocessorutils.go
+++ cdprocessorutils.go
@@ -163,7 +163,7 @@ func (s *Server) makeLinks(ctx context.Context, ID int32, force bool, config *pb
 	}
 
 	// Skip records which aren't in the listening pile
-	if record.GetRelease().GetFolderId() != 7664293 && record.GetRelease().GetFolderId() != 7651472 {
+	if record.GetRelease().GetFolderId() != 7664293 && record.GetRelease().GetFolderId() != 7651472 && record.GetRelease().GetFolderId() != 812802 {
 		s.CtxLog(ctx, "Skipping because it's not in the listening pile")
 		return nil
 	}
